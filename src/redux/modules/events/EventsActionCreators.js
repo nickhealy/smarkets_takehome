05069c45a7bcd5ad13ffaf26f82026c2cc2c4bb6
Nick Healy <nickrhealy@gmail.com>
2020-08-26 17:01:10 -0700

    rename variables in getEvents action

--- src/redux/modules/events/EventsActionCreators.js
+++ src/redux/modules/events/EventsActionCreators.js
@@ -4,10 +4,10 @@ const axios = require('axios');
 export const getEvents = sport => {
   // get the ids of the main events of a particular sport
   return async (dispatch) => {
-    const popularEvents = await axios.get('https://cors-anywhere.herokuapp.com/https://api.smarkets.com/v3/popular/event_ids/sport/tennis/'); 
-    const eventIds = popularEvents.data.popular_event_ids;
-    const eventsData = await axios.get(`https://cors-anywhere.herokuapp.com/https://api.smarkets.com/v3/events/${eventIds.toString()}/`); 
-    const events = eventsData.data.events; 
+    const popularEventsResponse = await axios.get('https://cors-anywhere.herokuapp.com/https://api.smarkets.com/v3/popular/event_ids/sport/tennis/'); 
+    const eventIds = popularEventsResponse.data.popular_event_ids;
+    const eventsDataResponse = await axios.get(`https://cors-anywhere.herokuapp.com/https://api.smarkets.com/v3/events/${eventIds.toString()}/`); 
+    const events = eventsDataResponse.data.events; 
 
     return dispatch({
       type: GET_EVENTS, 
